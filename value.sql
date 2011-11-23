920a0619804678b53be80ddb6e8896f6a87928a9
Ben Morrow <ben@morrow.me.uk>
2011-11-23 03:21:56 +0000

    ordered_first() and ordered_last().
    They have a prefix because they don't take an 'ordered' argument, so
    shadowing other functions becomes a problem.
    
    This completes the set of functions I was interested in, although there
    is no balancing and no gc yet. (first and last obviously produce very
    badly unbalanced trees.)

--- value.sql
+++ value.sql
@@ -8,22 +8,18 @@
 -- Released under the 2-clause BSD licence.
 --
 
-CREATE FUNCTION _new_ordered_val (ordered)
+CREATE FUNCTION _new_ordered_val (regclass)
     RETURNS ordered
     VOLATILE
     SET search_path FROM CURRENT
     LANGUAGE sql
     AS $$
         SELECT ROW(
-            $1.rel, nextval(
-                pg_get_serial_sequence(
-                    $1.rel::regclass::text, 'id'
-                )::regclass
-            )
+            $1, nextval(pg_get_serial_sequence($1::text, 'id'))
         )::ordered;
     $$;
 
-CREATE FUNCTION _insert (o ordered, before boolean)
+CREATE FUNCTION _before_after (o ordered, before boolean)
     RETURNS ordered
     VOLATILE
     SET search_path FROM CURRENT
@@ -34,7 +30,7 @@ CREATE FUNCTION _insert (o ordered, before boolean)
             prev    integer;
             subst   text[];
         BEGIN
-            rv      := _new_ordered_val(o);
+            rv      := _new_ordered_val(o.rel);
             subst   := ARRAY[ '$ord$', o.rel::regclass::text ];
 
             EXECUTE do_substs(subst, $$
@@ -67,11 +63,66 @@ CREATE FUNCTION before (ordered)
     VOLATILE
     SET search_path FROM CURRENT
     LANGUAGE sql
-    AS $$ SELECT _insert($1, TRUE) $$;
+    AS $$ SELECT _before_after($1, TRUE) $$;
 
 CREATE FUNCTION after (ordered)
     RETURNS ordered
     VOLATILE
     SET search_path FROM CURRENT
     LANGUAGE sql
-    AS $$ SELECT _insert($1, FALSE) $$;
+    AS $$ SELECT _before_after($1, FALSE) $$;
+
+CREATE FUNCTION _first_last (ord regclass, first boolean)
+    RETURNS ordered
+    VOLATILE
+    SET search_path FROM CURRENT
+    LANGUAGE plpgsql
+    AS $fn$
+        DECLARE
+            subst   text[];
+            rv      ordered;
+            old     integer;
+        BEGIN
+            subst   := ARRAY[ '$ord$', ord::text ];
+            rv      := _new_ordered_val(ord);
+
+            EXECUTE do_substs(subst, $old$
+                WITH RECURSIVE
+                    parent (id, depth) AS (
+                        SELECT id, 0 FROM $ord$ WHERE parent IS NULL
+                        UNION ALL
+                        SELECT c.id, p.depth + 1
+                            FROM $ord$ c JOIN parent p
+                                ON c.parent = p.id
+                            WHERE c.first = $1
+                    )
+                SELECT id FROM parent
+                    ORDER BY depth DESC
+                    LIMIT 1
+            $old$) INTO old USING first;
+
+            EXECUTE do_substs(subst, $ins$
+                INSERT INTO $ord$ (id, parent, first)
+                    VALUES (
+                        $1, $2, 
+                        CASE WHEN $2 IS NULL THEN NULL ELSE $3 END
+                    )
+            $ins$) USING rv.id, old, first;
+
+            RETURN rv;
+        END;
+    $fn$;
+
+CREATE FUNCTION ordered_first (regclass)
+    RETURNS ordered
+    VOLATILE
+    SET search_path FROM CURRENT
+    LANGUAGE sql
+    AS $$ SELECT _first_last($1, TRUE) $$;
+
+CREATE FUNCTION ordered_last (regclass)
+    RETURNS ordered
+    VOLATILE
+    SET search_path FROM CURRENT
+    LANGUAGE sql
+    AS $$ SELECT _first_last ($1, FALSE) $$;
